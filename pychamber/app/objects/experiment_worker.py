158410fbc0b37e0fe6e80f2c872b891dd9e2e640
Bailey Campbell <baileycampbell1990@gmaill.com>
2023-03-22 20:38:01 -0400

    Possible implementation of thread waiting

--- pychamber/app/objects/experiment_worker.py
+++ pychamber/app/objects/experiment_worker.py
@@ -5,12 +5,12 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from pychamber import positioner
 
+import functools
 import time
 
 import numpy as np
 import skrf
-from PySide6.QtCore import QObject, Signal
-from PySide6.QtTest import QSignalSpy
+from PySide6.QtCore import QEventLoop, QObject, QTimer, Signal
 
 
 class ExperimentWorker(QObject):
@@ -36,25 +36,22 @@ class ExperimentWorker(QObject):
         self.elevations = elevations
 
     def run(self) -> None:
-        print("Creating move spy")
-        move_spy = QSignalSpy(self.positioner.jogCompleted)
         iter_times = np.array([])
 
         total_completed = 0
         for az in self.azimuths:
-            print(f"Moving az to {az}")
-            self.positioner.move_az_absolute(az)
-            print("Waiting...")
-            move_spy.wait()
+            self.wait_for(
+                functools.partial(self.positioner.move_az_absolute, az), self.analyzer.jogCompleted, timeout=None
+            )
 
             cut_completed = 0
             for el in self.elevations:
-                print(f"Moving el to {el}")
                 start = time.time()
-                self.positioner.move_el_absolute(el)
-                print("Waiting...")
-                move_spy.wait()
-                print("Getting snp network")
+
+                self.wait_for(
+                    functools.partial(self.positioner.move_el_absolute, el), self.analyzer.jogCompleted, timeout=None
+                )
+
                 ntwk = self.analyzer.ch1.get_snp_network()
                 self.dataAcquired.emit(ntwk)
                 stop = time.time()
@@ -66,3 +63,18 @@ class ExperimentWorker(QObject):
                 self.avgIterTimeUpdated.emit(np.average(iter_times))
 
             self.totalIterCountUpdated.emit(total_completed)
+
+    def wait_for(self, fn: callable, signal, timeout: int | None = 5000) -> None:
+        event_loop = QEventLoop()
+        signal.connect(event_loop.quit)
+
+        if timeout is not None:
+            timer = QTimer()
+            timer.setSingleShot(True)
+            timer.timeout.connect(event_loop.quit)
+
+        fn()
+
+        if timeout is not None:
+            timer.start()
+        event_loop.exec()
