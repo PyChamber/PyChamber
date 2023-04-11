7c4e5f834775706bf9bf51b92b5059b61e5ee900
Bailey Campbell <baileycampbell1990@gmaill.com>
2023-04-11 10:49:57 -0400

    load plugins on import

--- pychamber/__init__.py
+++ pychamber/__init__.py
@@ -1,5 +1,10 @@
 from . import plugins, positioner
 from .calibration import Calibration
+from .experiment import Experiment
 from .experiment_result import ExperimentResult
+
+from .api import PluginManager
+
+PluginManager().load_plugins()
 
 __version__ = "0.1.0"
